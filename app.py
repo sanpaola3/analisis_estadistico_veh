7611c0fd9aa7a1a590f7e044be1332762168d0d5
sanpaola3 <paola.cs.acosta@gmail.com>
2024-10-10 14:57:28 -0600

    agregar carpetas

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import pandas as pd
-        import plotly.express as px
-        import streamlit as st
+import plotly.express as px
+import streamlit as st
         
-        car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-        hist_button = st.button('Construir histograma') # crear un botón
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
         
-        if hist_button: # al hacer clic en el botón
+if hist_button: # al hacer clic en el botón
             # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer")
         
             # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
