d13abadafb30796cc907108570b9c16486839dbd
sanpaola3 <paola.cs.acosta@gmail.com>
2024-10-10 20:04:05 -0600

    HISTOGRAMA

--- app.py
+++ app.py
@@ -14,3 +14,29 @@ if hist_button: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+# Botón para construir el histograma
+hist_button = st.button('Construir histograma')
+
+if hist_button:  # Al hacer clic en el botón
+    # Escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Botón para construir el gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:  # Al hacer clic en el botón
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Gráfico de dispersión: Odometer vs Price")
+
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
